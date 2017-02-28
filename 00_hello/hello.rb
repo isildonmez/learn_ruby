3b73d3d4069ff7a5b360bb01fa3fda2a3b4289b5
Isil Donmez <isil.donmez@gmail.com>
2017-02-28 19:02:31 +0200

    finished 00 & 01

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,9 @@
-#write your code here
+def hello
+	"Hello!"
+end
+
+def greet (who)
+	"Hello, #{who}!"
+end
+
+#  who vs Alice(while naming arguments) Variable names cannot begin with a capital letter. If an identifier begins with a capital letter, it is considered to be a constant in Ruby.
--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc (degree)
+	degree = (degree.to_f - 32) * 5 / 9
+	degree
+end
+
+def ctof (degree)
+	degree = degree.to_f * 9 / 5 + 32
+	degree
+end
