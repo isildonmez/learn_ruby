c6368ec17ac00bcd1697a78c2c1d29ffb6bc6623
Isil Donmez <isil.donmez@gmail.com>
2017-03-02 00:12:28 +0200

    Finished 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo string
+	return string
+end
+
+def shout string
+	return string.upcase
+end
+
+def repeat (string, no = 2)
+	(string.split * no).join(" ")
+end
+
+def start_of_word (string, no)
+	return string[0...no]
+end
+
+def first_word (string)
+	string.split[0]
+end
+
+def titleize (string)
+	array = string.capitalize.split
+	little_words = ["the", "over", "and"]
+	array.map! { |element|
+		if little_words.include?(element)
+			element
+		else
+			element.capitalize
+		end}
+	array.join(" ")
+end
