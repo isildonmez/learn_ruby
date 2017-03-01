57216aa06867fa7448afee2f7fa9d9c2e54e8d13
Isil Donmez <isil.donmez@gmail.com>
2017-03-01 17:55:25 +0200

    Finished 02: focused on inject method

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add (a,b)
+	a + b
+end
+
+def subtract (a,b)
+	a - b
+end
+
+def sum array
+	array.inject(0) {|result,x| result + x}
+end
+
+def multiply array
+	array.inject {|result,x| result * x }
+end
+
+def power (a,b)
+	a ** b
+end
+
+def factorial a
+	if a <= 1
+		return 1
+	else
+		(1..a).inject(1){|result, x| result * x}
+	end
+end
