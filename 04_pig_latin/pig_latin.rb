c39912525460d896f96064689d06aa93c3a170ce
Isil Donmez <isil.donmez@gmail.com>
2017-03-03 19:23:00 +0200

    Finished 04:used slice method

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
-#write your code here
+def translate (string)
+	vowels = ["a", "e", "i", "o", "u"]
+	array = string.split
+	array.map! {|word|
+		while !vowels.include? word[0] # while the 1stChr of each word begins with non vowel
+			if word[0..1] == "qu"
+				suffix = word.slice! word[0..1]
+			else
+				suffix = word.slice! word[0]
+			end
+			word += suffix
+		end
+	word += "ay"}
+	array.join(" ")
+end
