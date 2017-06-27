6f3a7f5b2fe1f9802ae76c0fba7449feb4b3da0b
Isil Donmez <isil.donmez@gmail.com>
2017-06-27 17:56:54 +0100

    Finished 05:used method with equal sign

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title=(string)
+		always_lower = %w(the a an and in of)
+		array = string.capitalize!.split
+		array.map! {|word|
+			if always_lower.include? word
+				word
+			else
+				word.capitalize
+			end
+		}
+		@title = array.join(" ")
+	end
 end
