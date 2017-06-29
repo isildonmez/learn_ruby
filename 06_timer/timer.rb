e35f5ae9b8455f0430a7851c5ffde7fc004748a9
Isil Donmez <isil.donmez@gmail.com>
2017-06-29 18:40:29 +0100

    Finished 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,30 @@
 class Timer
-  #write your code here
+	attr_accessor :seconds
+
+	def initialize
+		@seconds = 0
+	end
+
+	def time_string
+		tempSeconds = @seconds
+
+		if tempSeconds == 0
+			time = "00:00:00"
+		else
+			hour = (tempSeconds / 3600)
+			tempSeconds %= 3600
+			minutes = (tempSeconds / 60)
+			tempSeconds %= 60
+
+			def digits (twoDigits)
+				if twoDigits < 10
+					twoDigits = "0#{twoDigits}"
+				else
+					twoDigits.to_s
+				end
+			end
+
+			time = digits(hour) + ":" + digits(minutes) + ":" + digits(tempSeconds)
+		end
+	end
 end
